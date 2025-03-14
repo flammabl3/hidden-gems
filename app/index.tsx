652cbca837446fd5eca7d47cce97f0bd0285289d
Neeraj-Bachwani <NeerajPradeepkumar.Bachwani@edu.sait.ca>
2025-03-14 01:46:38 -0600

    added routing and added starting page

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'expo-router';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function Index(){
+    const router = useRouter();
+  
+    return (
+      <View>
+        <Text >This is the login page check check </Text>
+      </View>
+    );
+}
